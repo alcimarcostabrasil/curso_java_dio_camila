a9d31f606fc4389fae05a97c2b6247e2c3f5d553
Alcimar <alcimar.zeneide@gmail.com>
2022-10-08 13:01:23 -0300

    ajustes de funções

--- src/primeiro/Main.java
+++ src/primeiro/Main.java
@@ -1,5 +1,7 @@
 package primeiro;
 
+import java.util.Scanner;
+
 import primeiro.models.Agenda;
 import primeiro.models.Gato;
 import primeiro.models.Livro;
@@ -10,6 +12,16 @@ public class Main {
 		Agenda agenda = new Agenda("Marcos da silva", "82 09999-9999");
 		Livro livro = new Livro("Curso de Java para programadores Python", "Alcimar Costa", 900);
 		Gato gato = new Gato("Caldo de cana", 1);
+		
+		Scanner scan = new Scanner(System.in);
+		
+		int a, b, resultado;
+		System.out.println("Digite o primeiro valor: ");
+		a = scan.nextInt();
+		System.out.println("Digite o segundo valor: ");
+		b = scan.nextInt();
+		resultado = a + b;
+		System.out.println("O resultado da soma dos dois números é: " + resultado);
 		
 		System.out.println("Contato: " + agenda.getNome() + " - Telefone: " + agenda.getTelefone());
 		System.out.println("Livro: " + livro.getTitulo() + " - Autor: " + livro.getAutor());
